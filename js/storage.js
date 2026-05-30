7ea016b180182e9902f9b03c8869c3597373c9c5
HADOLIN <hadolinemlak@gmail.com>
2026-05-30 11:47:27 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,23 @@
+export const Storage = {
 
+    save(key, data) {
+
+        localStorage.setItem(
+            key,
+            JSON.stringify(data)
+        );
+    },
+
+    get(key) {
+
+        const data = localStorage.getItem(key);
+
+        return data
+            ? JSON.parse(data)
+            : [];
+    },
+
+    remove(key) {
+        localStorage.removeItem(key);
+    }
+};
