c5ac076be4142697da517a94bf8baba406f6ada1
HADOLIN <hadolinemlak@gmail.com>
2026-05-30 11:58:38 +0300

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,1 +1,27 @@
+import { savePhoto } from "./indexedDB.js";
 
+const photoInput =
+document.getElementById("photoInput");
+
+let uploadedPhotos = [];
+
+photoInput.addEventListener("change", async (e) => {
+
+    const files = [...e.target.files];
+
+    for (const file of files) {
+
+        const photoId =
+        crypto.randomUUID();
+
+        await savePhoto({
+
+            id: photoId,
+            file
+        });
+
+        uploadedPhotos.push(photoId);
+    }
+
+    console.log("Fotoğraflar kaydedildi");
+});
