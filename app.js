51bb5e32b3efe37d11cd0c30498a0b5c4c340dba
HADOLIN <hadolinemlak@gmail.com>
2026-05-31 12:06:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+// IndexedDB oluştur
+openDB()
 
+// Fotoğraf yükleme
+uploadPhoto()
+
+// Sıkıştırma
+compressImage()
+
+// Kaydetme
+savePhoto()
+
+// Listeleme
+getPhotos()
+
+// Silme
+deletePhoto()
+
+// Sayfa açılışında galeriyi yükle
+loadGallery()
